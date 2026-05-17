8c3ceb37fe10dca7e0266527f72f5e7fd6c2157c
marikoRw <rwabunezam@gmail.com>
2026-05-17 10:32:51 +0200

    adding the API fetch and using decode to structure data

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect } from "react"
+import { decode } from 'html-entities'
+
 export default function Quiz() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
+      .then(res => res.json())
+      .then(data => 
+        {
+          // console.log(data)
+          const quizData = data.results.map(q => {
+            const randomIndex = Math.floor(Math.random() * (q.incorrect_answers.length + 1))
+            const answers = [...q.incorrect_answers]
+            answers.splice(randomIndex, 0, q.correct_answer)
+            return { 
+              question: decode(q.question),
+              answers: answers
+            }
+          })
+          setQuestions(quizData)
+        })
+    }, [])
+    // console.log(questions)
+
   return (
-    <div>Quiz</div>
+    <div className="quiz-div">
+      {questions.map((q, index) => {
+        return (
+          <div key={index}>
+            <p>{q.question}</p>
+            <ul>
+              {q.answers.map((answer, i) => (
+                <button key={i}>{answer}</button>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
   )
 }
